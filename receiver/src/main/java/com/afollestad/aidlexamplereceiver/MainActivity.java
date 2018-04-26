c629ec0e53f3ad937517db1509fa3af568c1d147
wangsheng <wangsheng@gionee.com>
2018-04-26 17:57:55 +0800

    添加aidl demo

---
+++ receiver/src/main/java/com/afollestad/aidlexamplereceiver/MainActivity.java
@@ -0,0 +1,22 @@
+package com.afollestad.aidlexamplereceiver;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by ws on 18-4-26.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.main_layout);
+    }
+}
+
+
